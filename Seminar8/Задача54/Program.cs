a83ad99f47e875077548bfb39ea596b36ab5d4b1
elenapav <114250475+elenapav@users.noreply.github.com>
2022-11-14 13:36:05 +0300

    Задача54

---
+++ Seminar8/Задача54/Program.cs
@@ -0,0 +1,59 @@
+// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+int[,] SortStringDescendingOrder(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            for (int k = j + 1; k < inArray.GetLength(1); k++)
+            {
+                if (inArray[i, j] < inArray[i, k])
+                {
+                    int temp = inArray[i, j];
+                    inArray[i, j] = inArray[i, k];
+                    inArray[i, k] = temp;
+                }
+            }
+        }
+    }
+    return inArray;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите количество строк в массиве: ");
+int row = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов в массиве: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+int[,] array = GetArray(row, columns, 0, 9);
+Console.WriteLine("Исходная матрица");
+PrintArray(array);
+Console.WriteLine("Матрица, где элементы каждой строки упорядочены по убыванию");
+SortStringDescendingOrder(array);
+PrintArray(array);
